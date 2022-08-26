84e07d4346f5423d387cf5ce7f53841dd122c463
Komatsubara Masato <m-koma@nifty.com>
2022-08-27 00:12:33 +0900

    initial commit

---
+++ src/main/java/in/techcamp/issueapp/IssueRepository.java
@@ -0,0 +1,23 @@
+package in.techcamp.issueapp;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface IssueRepository {
+    @Insert("insert into issues (title, content, period, importance) values (#{title}, #{content}, #{period}, #{importance})")
+    void insert(String title, String content, String period, char importance);
+
+    @Select("select * from issues")
+    List<IssueEntity> findAll();
+
+    @Select("select * from issues where id = #{id}")
+    IssueEntity findById(long id);
+
+    @Update("UPDATE issues SET title = #{title}, content = #{content}, period = #{period}, importance = #{importance} WHERE id =#{id}")
+    void update(long id, String title, String content, String period, char importance);
+
+    @Delete("delete from issues where id=#{id}")
+    void deleteById(Long id);
+}
