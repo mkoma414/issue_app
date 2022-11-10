3c381e33dba4f852f960bddb6e4cffafaf90338a
Komatsubara Masato <m-koma@nifty.com>
2022-11-10 17:23:09 +0900

    railwayでデプロイ

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,7 +1,6 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
